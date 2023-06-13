e4c2270b577141fb49d48e555e7e3a721d506006
Bytebase <support@bytebase.com>
2023-06-13 20:47:33 +0800

    [Bytebase] Create latest schema for "test" after migration 202306130001-ddl
    THIS COMMIT IS AUTO-GENERATED BY BYTEBASE
    
    https://e745-2407-cdc0-b00d-00-421.ngrok-free.app/issue/test-alter-schema-create-table-t3-104?stage=104
    
    --------Original migration change--------
    
    https://github.com/bb-test-org/my-database/commit/db682dd839b93b0e970f814693289264e6cca00b
    
    Merge pull request #1 from bb-test-org/rebelice-patch-1
    
    Create test##202306130001##ddl##create_table_t3.sql

---
+++ bytebase/prod/.test##LATEST.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `t1` (
+  `a` INT(11) NOT NULL,
+  PRIMARY KEY (`a`)
+);
+
+CREATE TABLE `t3` (
+  `a` INT(11) DEFAULT NULL,
+  `b` INT(11) DEFAULT NULL
+);
+
+CREATE TABLE `user` (
+  `id` INT(10) NOT NULL,
+  `name` VARCHAR(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
